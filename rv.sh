24421a741d8ece38ecd8f17f7f57c630a59f94c5
root <yiyi.wang@runtimeverification.com>
2018-08-31 20:41:29 +0000

    Initial commit

---
+++ rv.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -x
+
+json_out=`pwd`/errors.json
+report_out=`pwd`/report
+rm -rf $json_out
+rm -rf $report_out
+
+apt install -y yasm
+
+mkdir -p ffmpeg-samples
+./configure --samples=ffmpeg-samples --cc=kcc --ld=kcc --extra-cflags="-fissue-report=$json_out" 
+make -j`nproc`
+make fate-rsync
+make check -j`nproc`
+
+touch $json_out && rv-html-report $json_out -o $report_out
+rv-upload-report $report_out
